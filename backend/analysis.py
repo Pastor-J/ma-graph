2456863090da70ca2c49193c335d7556533bd69c
Joa <betejoachim@gmail.com>
2025-01-10 21:08:52 +0100

    Added timeout in python backend for testing purposes

--- backend/analysis.py
+++ backend/analysis.py
@@ -1,7 +1,12 @@
+import time
 
 def analyze_result(flow_data):
   nodes = flow_data['nodes']
   edges = flow_data['edges']
+
+  print("Before sleep")
+  time.sleep(20)  # Pauses execution for 5 seconds
+  print("After sleep")
 
   # for edge in edges:
   #   print(edge)
