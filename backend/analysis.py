659ce02ae8c750ee19c74bb515a78168e230b70c
Joa <betejoachim@gmail.com>
2025-01-11 10:46:18 +0100

    Added some more logic to the analysis part

--- backend/analysis.py
+++ backend/analysis.py
@@ -4,8 +4,12 @@ import json
 
 llm = OllamaLLM(model="llama3.1:8b")
 
-def analyze_result(flow_data):
+def simple_analysis(flow_data):
   flow_data = json.dumps(flow_data)
-  prompt = flow_data + "Please analyse this graph. Answer in one sentence."
-  response = llm.invoke(prompt)
-  print(response)
+  query = f"""
+    You are a AI-Assitant for Failure Mode and Effects Analysis. Please analyse this graph: {flow_data}. Return a possible fault for node with id 4. Keep it as short as possible. Your ouput string should be maximum 5 words long.
+  """
+  for chunk in llm.stream(query):
+    print(chunk, end="", flush=True)
+  print("\n")
+
--- backend/backend.py
+++ backend/backend.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from analysis import analyze_result
+from analysis import simple_analysis
 
 app = Flask(__name__)
 CORS(app)
@@ -11,7 +11,7 @@ def analyze_flow():
     flow_data = request.get_json()
 
     # Analyze the flow data
-    analyze_result(flow_data)
+    simple_analysis(flow_data)
 
     # Store flow data into dictionary
     result = {"message": "Analysis complete", "data": flow_data}
